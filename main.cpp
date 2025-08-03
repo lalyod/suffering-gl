ad07cea33c016806e678feaed9aba37b75693e7b
lalyod <mpaproject307@gmail.com>
2025-08-03 17:12:59 +0700

    shader: put color inside vertex data

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
-#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 const char *vertexShaderSource = "#version 330 core\n"
 								 "layout (location = 0) in vec3 aPos;\n"
-								 "out vec4 vertexColor;\n"
+								 "layout (location = 1) in vec3 aColor;\n"
+								 "out vec3 ourColor;\n"
 								 "void main() {\n"
 								 "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-								 "	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+								 "	ourColor = aColor;\n"
 								 "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
 								   "out vec4 FragColor;\n"
-								   "uniform vec3 ourColor;\n"
+								   "in vec3 ourColor;\n"
 								   "void main() {\n"
 								   "	FragColor = vec4(ourColor, 1.0);"
 								   "}\0";
@@ -41,10 +41,10 @@ int main() {
 	}
 
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f
 	};
 
 	unsigned int indices[] = {
@@ -65,8 +65,11 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
@@ -117,12 +120,6 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-
-		float timeValue = glfwGetTime();
-		float greenValue = std::sin(timeValue) / 2.0f + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-		std::cout << "Greeen Value: "<< greenValue << std::endl;
 
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
